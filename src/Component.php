6c49b61f9630d4529dd750cd5bf4c4135a257e8d
Odin <odinuv@gmail.com>
2018-12-15 18:31:55 +0100

    feat: first version

--- src/Component.php
+++ src/Component.php
@@ -5,12 +5,44 @@ declare(strict_types=1);
 namespace MyComponent;
 
 use Keboola\Component\BaseComponent;
+use Keboola\Csv\CsvWriter;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class Component extends BaseComponent
 {
     public function run(): void
     {
-        // @TODO implement
+        $finder = new Finder();
+        $finder->in($this->getDataDir() . '/in/tables')->files();
+        $this->processDir($finder, 'tables');
+        $finder = new Finder();
+        $finder->in($this->getDataDir() . '/in/files')->files();
+        $this->processDir($finder, 'files');
+    }
+
+    private function processDir(Finder $finder, string $dir) : void
+    {
+        $fs = new Filesystem();
+        foreach ($finder as $inFile) {
+            if ($inFile->getExtension() === 'manifest') {
+                // copy manifest without modification
+                $fs->copy($inFile->getPathname(), $this->getDataDir() . "/out/$dir/" . $inFile->getFilename());
+            } else {
+                $destinationDir = $this->getDataDir() . "/out/$dir/" . $inFile->getRelativePath();
+                $fs->mkdir($destinationDir);
+                $destinationFile = $destinationDir . '/' . $inFile->getFilename();
+                $this->processFile($inFile, $destinationFile);
+            }
+        }
+    }
+
+    private function processFile(SplFileInfo $inFile, string $destinationFileName) : void
+    {
+        $csvFile = new CsvWriter($destinationFileName);
+        $csvFile->writeRow(['file']);
+        $csvFile->writeRow([(string) file_get_contents($inFile->getPathname())]);
     }
 
     protected function getConfigClass(): string
